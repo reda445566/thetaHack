d8c6c8d0585fee8ef0b04b2be41d692555f7b929
ibrahimwael951 <ibrahimwael809@gmail.com>
2026-08-10 07:24:49 +0300

    feat: initialize front-end project with Next.js and Tailwind CSS

---
+++ front-end/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Hello world</h1>
+      <h1 className="text-accent">Hello world</h1>
+    </main>
+  )
+}
